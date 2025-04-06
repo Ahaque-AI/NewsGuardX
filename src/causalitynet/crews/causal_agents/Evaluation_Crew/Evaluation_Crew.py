c73e3685ecbc5f800561886525aef0930b15b954
Abdul Haque <abdulhaque.dev@gmail.com>
2025-04-06 14:10:48 +0500

    Update Evaluation_Crew.py

--- src/causalitynet/crews/causal_agents/Evaluation_Crew/Evaluation_Crew.py
+++ src/causalitynet/crews/causal_agents/Evaluation_Crew/Evaluation_Crew.py
@@ -1,6 +1,6 @@
 from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
-from causalitynet.tools import custom_tool
+from src.causalitynet.tools import custom_tool
 
 # If you want to run a snippet of code before or after the crew starts,
 # you can use the @before_kickoff and @after_kickoff decorators
